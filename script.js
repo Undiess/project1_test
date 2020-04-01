8687a21ceec99bed0c76d040783eb5b21b4ea949
Andy Nguyen <nguyen.andy67@gmail.com>
2020-04-01 19:21:58 +1030

    adf

--- script.js
+++ script.js
@@ -47,7 +47,20 @@ img.style.transform = 'rotate('+heading+'deg)';
   */
 
  
-
+ function onClick() {
+  // feature detect
+  if (typeof DeviceOrientationEvent.requestPermission === 'function') {
+  DeviceOrientationEvent.requestPermission()
+  .then(permissionState => {
+  if (permissionState === 'granted') {
+  window.addEventListener('deviceorientation', () => {});
+  }
+  })
+  .catch(console.error);
+  } else {
+  // handle regular non iOS 13+ devices
+  }
+  }
 	
 
   window.addEventListener('deviceorientation', function(event) {
