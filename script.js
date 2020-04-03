219284c814eebe44c6b613827d01f478f54e8b90
Andy Nguyen <nguyen.andy67@gmail.com>
2020-04-03 21:55:18 +1030

    sd

--- script.js
+++ script.js
@@ -67,6 +67,14 @@ var Degrees = 0
   var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
   
   if (iOS === true){
+
+    var createButton = document.createElement("BUTTON");  
+    createButton.innerHTML = "CLICK ME";    
+    createButton.setAttribute("class","btn btn-outline-primary")   
+    createButton.setAttribute("id","but")
+    createButton.setAttribute("type","button")       
+    document.getElementsByClassName("compassdisplay")[0].appendChild(createButton); 
+
     /* https://stackoverflow.com/questions/16048514/can-i-use-javascript-to-get-the-compass-heading-for-ios-and-android
 stackoverflow was used to find the ollowing code to gain access to movement and orientation on ios devices*/
     var button = document.getElementById("but");
