29723000cb28fcdd5a0ccd8a5581c933b2782f5a
Andy Nguyen <nguyen.andy67@gmail.com>
2020-04-03 19:52:28 +1030

    asd

--- script.js
+++ script.js
@@ -46,24 +46,6 @@ img.style.transform = 'rotate('+heading+'deg)';
 
   */
 
-
-
- 
-
-
-var button = document.getElementById("but");
- button.addEventListener("click", function () {
-   console.log("hi")
-  DeviceMotionEvent.requestPermission().then(response => {
-    if (response == 'granted') {
-      window.addEventListener('devicemotion', (e) => {
-      // do something with e
-    })
-  }
-}).catch(console.error)
-});
-
-
 /*
 var Degrees = 0
 
@@ -78,21 +60,46 @@ var Degrees = 0
     update.innerHTML = (degrees)
    }, 500);
 */
-var compassdir;
-   if (window.DeviceOrientationEvent) {
-    // Listen for the deviceorientation event and handle the raw data
-    window.addEventListener('deviceorientation', function(eventData) {
-      
+
+ 
+
+
+function requestPermissionIOS(){
+  var iOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
   
+  if (iOS === true){
+    /* https://stackoverflow.com/questions/16048514/can-i-use-javascript-to-get-the-compass-heading-for-ios-and-android
+stackoverflow was used to find the ollowing code to gain access to movement and orientation on ios devices*/
+    var button = document.getElementById("but");
+    button.addEventListener("click", function () {
+    DeviceMotionEvent.requestPermission().then(response => {
+      if (response == 'granted') {
+         window.addEventListener('devicemotion', (e) => {
+        })
+     }
+    }).catch(console.error)
+    });
+  }
+}
+
+requestPermissionIOS();
+
+function GetOrientation(){
+  var compassdir;
+     if (window.DeviceOrientationEvent) {
+     window.addEventListener('deviceorientation', function(eventData) {
       if(event.webkitCompassHeading) {
-        // Apple works only with this, alpha doesn't work
         compassdir = event.webkitCompassHeading;  
       }
       else compassdir = event.alpha;
     });
   }
 
-  setInterval(function(){ 
-    var update = document.getElementById("degree")
-    update.innerHTML = (compassdir)
-   }, 500);
+    
+}
+setInterval(function(){ 
+  var update = document.getElementById("degree")
+  update.nnerHTML = (compassdir)
+}, 500);
+
+GetOrientation();
